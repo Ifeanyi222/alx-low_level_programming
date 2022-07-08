6e739b22aa61ac6d0427eb2dd9229d3da8a090d5
ifeanyi222 <linfitechitsolcom@gmail.com>
2022-07-08 16:56:08 -0700

    task_3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,8 +5,6 @@
  *
  *print_numbers - prints 0123456789\n
  *
- *
- *
  *Return: Always 0.
  *
  *Return: void
@@ -14,12 +12,13 @@
  */
 void print_numbers(void)
 {
-	char x;
-	for (x = 0; x <= 10; x++)
-{
-		_putchar(x + '0');
+    char x;
+    for (x = 0; x <= 10; x++)
+    {
+	_putchar(x + '0');
 
-	_putchar('\n');
+    }
+    _putchar('\n');
 return (0);
-
 }
+
