30f0d8bf568d65af9f54834202b8883231876fbf
ifeanyi222 <linfitechitsolcom@gmail.com>
2022-07-26 20:37:19 -0700

    function that splits a string into words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,86 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *word_len - Locates the index marking the end of the
+ *
+ *first word contained within a string.
+ *
+ *@str: The string to be searched.
+ *
+ *
+ *
+ *Return: The index marking the end of the initial word pointed to by str.
+ *
+ */
+int count_word(char *s)
+{
+	int flag, c, w;
+
+	flag = 0;
+	w = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
+	}
+
+	return (w);
+}
+/**
+ *count_words - Counts the number of words contained within a string.
+ *
+ *@str: The string to be searched.
+ *
+ *
+ *
+ *Return: The number of words contained within str.
+ *
+ */
+char **strtow(char *str)
+{
+	char **matrix, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
+
+	while (*(str + len))
+		len++;
+	words = count_word(str);
+	if (words == 0)
+		return (NULL);
+
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
+			{
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
+	}
+
+	matrix[k] = NULL;
+
+	return (matrix);
+}
