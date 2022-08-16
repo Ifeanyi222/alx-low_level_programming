5b3d627d3e6d766644f78546bfbc4fc816651e99
ifeanyi222 <linfitechitsolcom@gmail.com>
2022-08-16 00:56:13 -0700

    task3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,10 +1,12 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - that add a new node at the end
+ * add_nodeint_end - add a new node at the end
  * of a linked list
- * 
- * Return: the address of the new element. NUll if it failed.
+ * @head: head of a list.
+ * @n: n element.
+ *
+ * Return: address of the new element. NUll if it failed.
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
