a6e69d84c9516cc33a3d0c66e94d825f84071108
ifeanyi222 <linfitechitsolcom@gmail.com>
2022-08-16 01:01:42 -0700

    task6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 
 /**
- * pop_listint - that deletes the head node of
- * a linked list.
+ * pop_listint - deletes the head node of
+ * a linked list
+ * @head: head of a list.
  *
- * Return: head node's data(n).
+ * Return: head node's data.
  */
 int pop_listint(listint_t **head)
 {
