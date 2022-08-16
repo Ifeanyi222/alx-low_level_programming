54603a151e54e42f34b38d70f6cf0e944430051d
ifeanyi222 <linfitechitsolcom@gmail.com>
2022-08-16 00:58:46 -0700

    task5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 
 /**
- * free_listint2 - that frees a linked list.
+ * free_listint2 - frees a linked list
+ * @head: head of a list.
  *
- * Return: no.
+ * Return: no return.
  */
 void free_listint2(listint_t **head)
 {
