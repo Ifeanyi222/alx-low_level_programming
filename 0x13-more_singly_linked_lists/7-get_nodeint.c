05660b4e582e05d64d70bf87f1266069d5b67df6
ifeanyi222 <linfitechitsolcom@gmail.com>
2022-08-16 01:04:19 -0700

    task7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,9 +1,11 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - that returns the nth node of a linked list
+ * get_nodeint_at_index - returns the nth node of a linked list
+ * @head: head of a list.
+ * @index: index of the node.
  *
- * Return: the nth node. If node does not exist, returns NULL.
+ * Return: nth node. If node does not exist, returns NULL.
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
@@ -16,5 +18,3 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	return (head);
 }
-Footer
-
