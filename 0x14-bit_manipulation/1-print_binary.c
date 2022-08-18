41145f44f48b9a53b28fdd9289db06f8a8e12f31
ifeanyi222 <linfitechitsolcom@gmail.com>
2022-08-18 13:35:07 -0700

    task1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * print_binary - that prints the binary representation
+ * print_binary - prints the binary representation
  * of a number.
- *
  * @n: unsigned long int.
  *
  * Return: no return.
